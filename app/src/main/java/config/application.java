2d2f05b0e24af1cf8d82ad4788a0a412e1c0b3cd
omid moradzadeh <omid.moradzadeh@outlook.com>
2018-02-05 00:26:44 +0330

    Rejected commit: Default

--- app/src/main/java/config/application.java
+++ app/src/main/java/config/application.java
@@ -10,21 +10,15 @@ import DTO.Show;
 
 
 
-public class applicationConfig extends Application {
+public class application extends Application {
 
     ArrayList<Show> showArrayList = new ArrayList<Show>();
+
     public void setShows(ArrayList<Show> shows) {
         this.showArrayList = shows;
     }
+
     public ArrayList<Show> getShows() {
         return this.showArrayList;
-    }
-
-    ArrayList<ImageView> imageViews = new ArrayList<ImageView>();
-    public void setImageViews(ArrayList<ImageView> imageViews) {
-        this.imageViews = imageViews;
-    }
-    public ArrayList<ImageView> getImageViews() {
-        return this.imageViews;
     }
 }
